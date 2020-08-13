ffd69c8d11870acc988a6a7678dcd1c0d896529c
aleksandra-b-t <a.bystranowska.takahashi@gmail.com>
2020-08-13 19:09:40 -0400

    almost done

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :id, :name, :email, :gender, :age, :password_digest 
+  attributes :id, :name, :email, :gender, :age, :password_digest, :activities
   has_many :steps
-  has_many :activities
+  # has_many :activities
 end
